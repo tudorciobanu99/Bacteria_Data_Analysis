ee5580b40e3c93b845bf4f02d752dcf0dd64a886
Tudor Ciobanu <31960334+tudorciobanu99@users.noreply.github.com>
2018-11-01 17:01:26 +0100

    Add files via upload

--- data_statististics.py
+++ data_statististics.py
@@ -16,26 +16,26 @@ def dataStatistics(data, statistic):
                            "Rows", "Mean Cold Growth rate", "Mean Hot Growth rate"])
     
     if (statistic == statistics[0]):
-        result = np.average(data[:,0]) 
+        result = np.average(data[:,0].astype(np.float)) 
         
     elif (statistic == statistics[1]):
-        result = np.average(data[:,1])
+        result = np.average(data[:,1].astype(np.float))
         
     elif (statistic == statistics[2]):
-        result = np.std(data[:,0])
+        result = np.std(data[:,0].astype(np.float))
         
     elif (statistic == statistics[3]):
-        result = np.std(data[:,1])
+        result = np.std(data[:,1].astype(np.float))
         
     elif (statistic == statistics[4]):
         result = np.size(data, 0)
         
     elif (statistic == statistics[5]):
         indices = np.where((data[:,0]).astype(np.float) < 20)  # This array holds the indices for the lines which contain a temperature less than 20 degrees.
-        result = np.average(data[:,1][indices], axis = 0)
+        result = np.average(data[:,1][indices].astype(np.float), axis = 0)
     elif (statistic == statistics[6]):
         
         indices = np.where((data[:,0]).astype(np.float) > 50)
-        result = np.average(data[:,1][indices], axis = 0)sssa    
+        result = np.average(data[:,1][indices].astype(np.float), axis = 0)   
         
     return result
